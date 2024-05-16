5d59fa114d115242231bab12039acd6444f7d115
MostafaEid-2019053 <mostafaeed70@gmail.com>
2024-05-16 16:09:18 +0300

    Initial Commit

---
+++ Add two numbers/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Add two numbers
+//
+//  Created by Mostafa Eed on 5/16/24.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
